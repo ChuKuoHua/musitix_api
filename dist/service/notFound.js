6708ca19823933852ad703e9ee92301e0756e24f
ChuKuoHua <enjoy12589@gmail.com>
2023-05-02 09:57:56 +0800

    delect dist

--- dist/service/notFound.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const notFound = (req, res, next) => {
-    res.status(404).send({
-        "status": "false",
-        "massage": '無此路由'
-    });
-};
-exports.default = notFound;
