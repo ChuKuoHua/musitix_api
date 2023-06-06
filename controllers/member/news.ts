6d48d20056344d26784893482d8cf184d073ec02
tcpyoung <tcpyoung4875@gmail.com>
2023-06-06 21:10:34 +0800

    [修改] 客戶端 最新消息API

--- controllers/member/news.ts
+++ controllers/member/news.ts
@@ -2,12 +2,18 @@ import { Request, Response, NextFunction } from 'express';
 import handleSuccess from '../../service/handleSuccess';
 import LatestNews from '../../models/latestnewsModel';
 
-const latestNews = {
+const latestNew = {
   // GET 最新消息
   async getLatestNews(req: Request, res: Response, next: NextFunction) {
     const latestNews = await LatestNews.find().lean();
-    handleSuccess(res, latestNews);
+    const Result = latestNews.map(latestNews => ({
+      id: latestNews._id,
+      title: latestNews.title,
+      date: latestNews.updatedAt,
+      content: latestNews.content,
+    }));
+    handleSuccess(res, Result);
   }
 }
 
-export default latestNews;
+export default latestNew;
